6cf1b0a217e8bbd1499933aeaab1a9015c40c31e
advait0012 <advait0012@proton.me>
2025-01-06 02:09:47 +0530

    home page done

---
+++ app/gens/page.jsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+import Header from "@/components/Header";
+function page() {
+  return (
+    <div className="h-screen bg-yellow-300 px-10 overflow-hidden">
+      <Header />
+      <section className="p-10 grid grid-cols-2 md:grid-cols-3 gap-12 drop-shadow-2xl place-items-center">
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-110 hover:transition-all">
+          Generation 1
+        </div>
+
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-110 hover:transition-all">
+          Generation 2
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-110 hover:transition-all">
+          Generation 3
+        </div>
+        <div className=" element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-110 hover:transition-all">
+          Generation 4
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-110 hover:transition-all">
+          Generation 5
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-110 hover:transition-all">
+          Generation 6
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-110 hover:transition-all">
+          Generation 7
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-110 hover:transition-all">
+          Generation 8
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-100 hover:transition-all">
+          Generation 9
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-100 hover:transition-all">
+          Generation 10
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-100 hover:transition-all">
+          Generation 11
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-100 hover:transition-all">
+          Generation 12
+        </div>
+        <div className="element solid border border-zinc-700 text-red-600 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-xl md:text-3xl px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 cursor-pointer hover:scale-100 hover:transition-all">
+          Generation 13
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default page;
